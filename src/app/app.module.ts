69afbddadf3eecd6583a640c51fc898fe98d9822
Kelvin Hsu <skps0102@gmail.com>
2022-06-19 22:33:57 +0800

    將 Component 拆分&套版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
+import { TagComponent } from './tag/tag.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleComponent,
+    TagComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
